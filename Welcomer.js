e38e2973fb5e8549b53e61b0e64c3e030b331a26
Danny <96847747+BLNDaniel@users.noreply.github.com>
2024-05-14 17:19:09 +0200

    Update Welcomer.js

--- Welcomer.js
+++ Welcomer.js
@@ -4,13 +4,13 @@ const client = new Discord.Client();
 const CHANNEL_ID = '1216431770396921948';
 
 client.on('ready', () => {
-    console.log(`Bot ist bereit als: ${client.user.tag}!`);
+    console.log(`Bot is ready: ${client.user.tag}!`);
 });
 
 client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.cache.get(CHANNEL_ID);
     if (!channel) return;
-    channel.send(`Willkommen auf dem Server, ${member}`);
+    channel.send(`Welcome, ${member}`);
 });
 
 client.login('BOT TOKEN');
